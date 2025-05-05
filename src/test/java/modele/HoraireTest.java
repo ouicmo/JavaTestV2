969cae0b99244785e0ea08ce00db3c58cc8a9145
Termeche_Heddi <hedditer@gmail.com>
2025-05-05 12:11:22 +0200

    1er changement

--- src/test/java/modele/HoraireTest.java
+++ src/test/java/modele/HoraireTest.java
@@ -8,16 +8,25 @@ class HoraireTest {
 
     @Test
     void compareToAnterieur() {
+        Horaire h1 = new Horaire (8,0);
+        Horaire h2 = new Horaire (8,15);
+        assertTrue(h1.compareTo(h2) < 0);
 
     }
 
     @Test
     void compareToPosterieur() {
+        Horaire h1 = new Horaire (10,20);
+        Horaire h2 = new Horaire (10,0);
+        assertTrue(h1.compareTo(h2) > 0);
 
     }
 
     @Test
     void compareToEgal(){
+        Horaire h1 = new Horaire (8,0);
+        Horaire h2 = new Horaire (8,0);
+        assertTrue(h1.compareTo(h2) == 0);
 
     }
 }
