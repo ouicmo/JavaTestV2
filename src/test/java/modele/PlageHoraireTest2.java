51823b2abe55a1bcf5094bf1fcb8048a65172299
Termeche_Heddi <hedditer@gmail.com>
2025-05-05 11:53:24 +0200

    1er changement

--- src/test/java/modele/PlageHoraireTest2.java
+++ src/test/java/modele/PlageHoraireTest2.java
@@ -6,9 +6,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
-class PlageHoraireTest {
+class PlageHoraireTest2 {
 
-    @org.junit.jupiter.api.Test
+    @Test
 
         @Order(1)
         void testPlageAvant() {
